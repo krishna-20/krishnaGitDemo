9d71e619ca384ba683cdee29ff4d4ff52d1e5265
krishna <tripathikk17@gmail.com>
2019-04-15 04:31:47 +0530

    this is my first commit

---
+++ src/main/java/krishna/test/App.java
@@ -0,0 +1,16 @@
+package krishna.test;
+
+import org.testng.annotations.Test;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+	@Test
+   public void test()
+    {
+        System.out.println( "Hello World!" );
+    }
+}
